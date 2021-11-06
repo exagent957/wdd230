23f3669def3dafbff95d12d645fc62dfb214acdc
exagent957 <exagent957@gmail.com>
2021-11-05 19:56:33 -0500

    211105

--- lesson8/js/validateInput.js
+++ lesson8/js/validateInput.js
@@ -1,7 +1,7 @@
 //Validate Input - only alphas spaces minimum of 5 characters
 function allAlphas() {
   const inputText = document.getElementById("fullname").value;
-  const pattern = /^[A-Za-z ]{5,30}$/;
+  const pattern = /^[A-Za-z .-]{5,30}$/;
   if (pattern.test(inputText)) {
     return true;
   } else {
