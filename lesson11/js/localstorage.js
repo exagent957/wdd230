b800017a0af32f4cc19d5713fd24aee8fb5e5510
exagent957 <exagent957@gmail.com>
2021-11-19 16:14:41 -0600

    211119E

--- lesson11/js/localstorage.js
+++ lesson11/js/localstorage.js
@@ -1,13 +1,11 @@
 /*WDD230 Lesson 7 - Gallery page. Calculates number of days since last visit to gallery page using local storage.*/
 
 const currentVisit = new Date().getTime(); //get current milliseconds
-console.log(currentVisit);
+
 //Access localStorage
 const myStorage = window.localStorage;
 
-console.log(myStorage);
 //check for lastVisit in localStorage
-console.log(window.localStorage.getItem("lastVisit"));
 if (myStorage.getItem("lastVisit")) {
   let previousVisit = myStorage.getItem("lastVisit");
   //subtract miliseconds and divide by 86400000 to convert to days - truncate to lowest whole day
