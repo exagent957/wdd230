8940cad21fcfae182e6d38936e5de7fed323f9e5
exagent957 <exagent957@gmail.com>
2021-12-12 12:19:55 -0600

    211212A

---
+++ hartsellechamber/js/toggleMenu.js
@@ -0,0 +1,4 @@
+//function to toggle primaryNav class on and off
+function toggleMenu() {
+  document.querySelector("#primaryNav").classList.toggle("hide");
+}
--- hartsellechamber/js/weather.js
+++ hartsellechamber/js/weather.js
@@ -45,7 +45,8 @@ fetch(apiURL)
         .setAttribute("alt", forecastDescription);
     }
     /*Show National Weather Alert if there is one*/
-    jsObject.alerts = [
+    /****for CODE TESTING
+     jsObject.alerts = [
       {
         description: "ALERT 1 - Hard freeze tonight",
       },
@@ -53,7 +54,7 @@ fetch(apiURL)
         description:
           "ALERT 2 ...HEAT ADVISORY REMAINS IN EFFECT FROM 1 PM THIS AFTERNOON TO\n8 PM CDT THIS EVENING...\n* WHAT...Heat index values of 105 to 109 degrees expected.\n* WHERE...Creek, Okfuskee, Okmulgee, McIntosh, Pittsburg,\nLatimer, Pushmataha, and Choctaw Counties.\n* WHEN...From 1 PM to 8 PM CDT Thursday.\n* IMPACTS...The combination of hot temperatures and high\nhumidity will combine to create a dangerous situation in which\nheat illnesses are possible.",
       },
-    ];
+    ]; */
 
     if (jsObject.alerts) {
       document.querySelector(".weather-alert-container").style.display =
