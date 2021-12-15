d8aea1737f67c6398956a109b01816e4097b75e9
exagent957 <exagent957@gmail.com>
2021-12-15 15:39:15 -0600

    211215A

--- hartsellechamber/js/bizdirectory.js
+++ hartsellechamber/js/bizdirectory.js
@@ -6,7 +6,7 @@ fetch(jsonURL)
     console.log(jsObject);
     const members = jsObject["members"];
     for (let i = 0; i < members.length; i++) {
-      //build the business member cards using
+      //build the business member cards
       let card = document.createElement("section");
       let bizlogo = document.createElement("img");
       let bizname = document.createElement("h2");
@@ -28,8 +28,37 @@ fetch(jsonURL)
       website.setAttribute("href", `${members[i].website}`);
       website.textContent = `Website: ${members[i].bizname}`;
       card.appendChild(website);
-      card.setAttribute("class", "grid-view");
+      card.classList.add("grid-view");
       document.querySelector("div.bizcard-container").appendChild(card);
+      document
+        .querySelector("div.bizcard-container")
+        .classList.add("grid-view");
       console.log(card);
     }
   });
+
+function gridView() {
+  const container = document.querySelector("div.bizcard-container");
+  console.log(container);
+  container.classList.add("grid-view");
+  container.classList.remove("list-view");
+  const cards = document.querySelectorAll("div.bizcard-container section");
+  console.log(cards);
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].classList.add("grid-view");
+    cards[i].classList.remove("list-view");
+  }
+}
+
+function listView() {
+  const container = document.querySelector("div.bizcard-container");
+  console.log(container);
+  container.classList.add("list-view");
+  container.classList.remove("grid-view");
+  const cards = document.querySelectorAll("div.bizcard-container section");
+  console.log(cards);
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].classList.add("list-view");
+    cards[i].classList.remove("grid-view");
+  }
+}
